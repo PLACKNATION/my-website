48855740d9d1ef2c6dbec2640054dc558dd407a1
plack <mannyflory27@gmail.com>
2021-09-15 00:35:44 -0400

    security feature

--- my_website/my_website/settings.py
+++ my_website/my_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7n=o-iv9v@0w95%umjf^7(3r2@-&#j=35xjq)!#_molm8j@^l*'
+with open(os.path.join(BASE_DIR, 'llavedesecreto.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
