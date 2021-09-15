6a5356d98c0133f8330f68060ba9417e16364696
plack <mannyflory27@gmail.com>
2021-09-15 01:31:03 -0400

    set up templates

--- my_website/home/views.py
+++ my_website/home/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home_page(request):
     context = {}
-    return render(request, "home/base.html", context)
+    return render(request, 'home/home_page.html', context)
 
-def about_page(requests):
-    return HttpResponse("<h1> About Page </h1>")
+def about_page(request):
+    context = {}
+    return render(request, 'home/base.html', context)
 
-def contact_page(requests):
-    return HttpResponse("<h1> Contact Page </h1>")
+def contact_page(request):
+    context = {}
+    return render(request, 'home/base.html', context)
--- my_website/my_website/settings.py
+++ my_website/my_website/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 with open(os.path.join(BASE_DIR, 'llavedesecreto.txt')) as f:
@@ -35,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
